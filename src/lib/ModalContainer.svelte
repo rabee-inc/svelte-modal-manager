1f049428c526343cba6b91dcffda5d9b917ef683
suico-jp <ryoorer@gmail.com>
2022-03-04 15:35:04 +0900

    fix

--- src/lib/ModalContainer.svelte
+++ src/lib/ModalContainer.svelte
@@ -91,38 +91,24 @@
     height: 100%;
   }
   .f {
-    display: -webkit-flex;
-    display: -moz-flex;
     display: flex;
   }
   .fc {
-    -webkit-justify-content: center;
-    -moz-justify-content: center;
     justify-content: center;
   }
+  .fl {
+    justify-content: flex-start;
+  }
+  .fr {
+    justify-content: flex-end;
+  }
   .fm {
-    -webkit-align-items: center;
-    -moz-align-items: center;
     align-items: center;
   }
   .ft {
-    -webkit-align-items: flex-start;
-    -moz-align-items: flex-start;
     align-items: flex-start;
   }
-  .fr {
-    -webkit-justify-content: flex-end;
-    -moz-justify-content: flex-end;
-    justify-content: flex-end;
-  }
   .fb {
-    -webkit-align-items: flex-end;
-    -moz-align-items: flex-end;
     align-items: flex-end;
-  }
-  .fl {
-    -webkit-justify-content: flex-start;
-    -moz-justify-content: flex-start;
-    justify-content: flex-start;
   }
 </style>
