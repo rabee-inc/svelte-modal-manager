0e0c63406eba9b5dff05964a95e931b33f523aba
phi <phi1618jp@gmail.com>
2023-04-18 19:03:46 +0900

    setup adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,28 +1,16 @@
 import path from 'path';
-import adapterAuto from '@sveltejs/adapter-auto';
-import adapterNode from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-auto';
 import sveltePreprocess from 'svelte-preprocess';
 const preprocess = sveltePreprocess({
   typescript: true,
   // ...
 });
-let adapter = null;
-
-if (adapterAuto().name !== '@sveltejs/adapter-auto') {
-  // vercel/netlify/cloudflare pages
-  adapter = adapterAuto();
-}
-else {
-  // auto で hit しなかったら強制的に node にする
-  adapter = adapterNode();
-}
-
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter,
-
+    adapter: adapter(),
+    
     alias: {
       'svelte-modal-manager': path.resolve('./src/lib'),
       $components: path.resolve('./src/components'),
